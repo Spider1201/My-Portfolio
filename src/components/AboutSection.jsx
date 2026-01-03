3b9e23d75e797a4fc6d34783baafbc2be413e9b0
Onatade Adeyincka Abdulmajeed <onatadeabdulmajeed@gmail.com>
2026-01-03 11:26:02 +0100

    Added the Download CV button

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -22,7 +22,9 @@ const AboutSection = () => {
             </p>
             <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
               <a href="#contact" className='cosmic-button  '>Get In Touch</a>
-              <a className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'>Download CV</a>
+              <a href="/01-0natade-resume.pdf" 
+                 download
+                 className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'>Download CV</a>
             </div>
           </div>
           <div className='grid grid-cols-1 gap-6'>
