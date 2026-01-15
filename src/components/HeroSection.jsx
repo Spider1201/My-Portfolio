b3b87f270624610e8a71c670e6b28f70d4e7f70c
Onatade Adeyincka Abdulmajeed <onatadeabdulmajeed@gmail.com>
2026-01-15 21:30:56 +0100

    Changed the spelling of some word in my hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
       <div className='container max-w-4xl mx-auto text-center z-10'>
         <div className='space-y-6'>
           <h1 className='text-4xl md:text-6xl font-bold tracking-tight'>  
-            <span className='opacity-0 animate-fade-in'>Hi, i'm </span>
+            <span className='opacity-0 animate-fade-in'>Hi, I'm </span>
             <span className='text-primary opacity-0 animate-fade-in-delay-1'>Abdulmajeed</span>
             <span className='text-gradient ml-2 opacity-0 animate-fade-in-delay-2'></span>
           </h1>
           <p className='text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3 '>
-            i create modern and responsive web applications and also native application for IOS and Andriod  using the latest technologies to deliver exceptional user experiences.
+            i create modern and responsive web applications and also native application for IOS and Android  using the latest technologies to deliver exceptional user experiences.
           </p>
           <div className='pd-4 opacity-0 animate-fade-in-delay-4'>
             <a href="#projects" className='cosmic-button'>
